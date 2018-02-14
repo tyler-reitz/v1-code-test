10ff33a52a6d617c993bb25cfbb54f716345550b
Tyler Reitz <tdreitz@gmail.com>
2018-02-14 09:29:07 -0800

    add client and cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 import schema from './data/schema';
 
-const GRAPHQL_PORT = 3000;
+const GRAPHQL_PORT = 4000;
 
 const graphQLServer = express();
+graphQLServer.use(cors())
 
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
