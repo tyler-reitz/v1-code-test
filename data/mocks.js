58a6d04af574cb65ccd22f818fd352ff89bd2eb7
Tyler Reitz <tdreitz@gmail.com>
2018-02-14 08:52:14 -0800

    mock server setup

--- data/mocks.js
+++ data/mocks.js
@@ -1,5 +1,19 @@
+import { MockList } from 'graphql-tools'
+import casual from 'casual'
+
 const mocks = {
-  String: () => 'It works!'
+  String: () => 'It works!',
+  Product: () => ({
+    id: () => casual.integer(0, 1000),
+    name: () => casual.color_name,
+    price: () => casual.double(0, 250)
+  }),
+  Collection: () => ({
+    id: () => casual.integer(0,1000),
+    desc: () => 'Pantone colors 2018',
+    products: () => new MockList([10,20]),
+    discount: () => casual.double(0, 100),
+  })
 };
 
 export default mocks;
--- data/schema.js
+++ data/schema.js
@@ -2,8 +2,21 @@ import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import mocks from './mocks';
 
 const typeDefs = `
+type Collection {
+  id: Int
+  desc: String
+  products: [Product]
+  discount: Float
+}
+type Product {
+  id: Int
+  name: String
+  price: Float
+  collection: Collection
+}
 type Query {
-  testString: String
+  collection(id: Int): Collection
+  product(name: String): Product
 }
 `;
 
